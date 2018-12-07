b2783e44b51346a16d6604f33e1a37c32cf6c730
Z1mm3r <czimmer@ramapo.edu>
2018-12-07 11:14:04 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      return  { friends: [...state.friends,action.friend]}
+    case "REMOVE_FRIEND":
+      let index = state.friends.findIndex((element) => {
+        return element.id === action.id
+      })
+      return {friends:[...state.friends.slice(0,index),
+              ...state.friends.slice(index + 1)]}
+    default:
+      return state
+  }
 }
