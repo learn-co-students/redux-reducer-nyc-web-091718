68797947614e5f09d438abc81f7b16ed901c6a4e
asc5025 <albert.s.chun@gmail.com>
2018-12-31 09:53:46 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+const initialState = {
+  friends: []
+}
+
+export function manageFriends(state = initialState, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends:[...state.friends, action.friend] }
+    case "REMOVE_FRIEND":
+      const newArray = state.friends.filter(f => f.id !== action.id)
+      return {...state, friends: newArray }
+    default: return state
+
+  }
 }
